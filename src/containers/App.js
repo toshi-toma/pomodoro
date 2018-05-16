0ab7a7ac9f30e4666f8d90a6fdd95f4350c7e1c6
toshi-toma <toshitoma1217@gmail.com>
2018-05-16 23:51:50 +0900

    Impl stationery for the Top page

---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => (
+  <div className="containers">
+    <div className="sidebar-container" />
+    <div className="timer-container" />
+    <div className="menu-container" />
+  </div>
+)
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
+import App from './containers/App'
 
-render(<p>Hello Pomodoro Timer.</p>, document.getElementById('root'))
+render(<App />, document.getElementById('root'))

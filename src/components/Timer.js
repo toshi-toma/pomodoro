4e30f26010c03ebd5a34025c1a41df762c68fb4b
toshi-toma <toshitoma1217@gmail.com>
2018-05-17 23:20:39 +0900

    Impl stationery for the Timer Component

---
+++ src/components/Timer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const defaultTimer = '25:00'
+
+const Timer = () => <h2>{defaultTimer}</h2>
+
+export default Timer
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import Timer from '../components/Timer'
 
 const App = () => (
   <div className="containers">
     <div className="sidebar-container">
       <Sidebar />
     </div>
-    <div className="timer-container" />
+    <div className="timer-container">
+      <Timer />
+    </div>
     <div className="menu-container" />
   </div>
 )

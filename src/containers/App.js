9dd850ce2480538ccf55e1c7891bf9c7f78b7ae9
toshi-toma <toshitoma1217@gmail.com>
2018-06-02 20:30:25 +0900

    wrap App component by Provider

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import PropTypes from 'prop-types';
 import Sidebar from '../components/Sidebar';
 import Timer from '../components/Timer';
 import Button from '../components/Button';
 
-const App = () => (
-  <div className="containers">
-    <div className="sidebar-container">
-      <Sidebar />
+const App = ({ store }) => (
+  <Provider store={store}>
+    <div className="containers">
+      <div className="sidebar-container">
+        <Sidebar />
+      </div>
+      <div className="timer-container">
+        <Timer />
+      </div>
+      <div className="menu-container">
+        <Button />
+      </div>
     </div>
-    <div className="timer-container">
-      <Timer />
-    </div>
-    <div className="menu-container">
-      <Button />
-    </div>
-  </div>
+  </Provider>
 );
+
+App.propTypes = {
+  store: PropTypes.shape({}).isRequired
+};
 
 export default App;
